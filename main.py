a4e9ff043f9dec7097c001bc531714bcd9611317
Simon Baumeler <denis_99@hotmail.ch>
2023-10-19 10:26:42 +0200

    Initial implementation fast api

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-# main.py
+from fastapi import FastAPI, Response, status
 
-print("Hello, world!")
+app = FastAPI()
+
+
+@app.post("/select_trump")
+async def select_trump():
+    response = Response()
+    #select trump
+    response.status_code = status.HTTP_200_OK
+    return response
+
+
+@app.post("/play_card")
+async def play_card():
+    response = Response()
+    #play card
+    response.status_code = status.HTTP_200_OK
+    return response
+
+
+@app.post("/game_info")
+async def game_info():
+    response = Response()
+    # process game info
+    response.status_code = status.HTTP_200_OK
+    return response
+
+
+
