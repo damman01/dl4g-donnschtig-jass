d91b7bc11ec8c60f5c3272ccab7719b8a2845949
Ammann Dominic damman01 <dominic.ammann@stud.hslu.ch>
2023-12-15 01:01:27 +0100

    refactor get_train_data function and improve data handling
    The get_train_data function in play_training_data_prep_json.py has been refactored to optimize data handling. The changes include:
    
    - Converting the played_card value to a one-hot encoded representation using the keras.utils.to_categorical function, improving compatibility with Tensorflow/Keras models.
    - Concatenating the list of DataFrames into a single DataFrame using pd.concat for improved data organization and analysis.
    - Reshaping the resulting DataFrame into a numpy array with a shape of (82,), ensuring compatibility with downstream operations.
    
    These changes enhance the efficiency and compatibility of data processing for training play data. No relevant issues are associated with this commit.

--- training_play/play_training_data_prep_json.py
+++ training_play/play_training_data_prep_json.py
@@ -5,18 +5,28 @@ import json
 import glob
 from jass.game.game_observation import GameObservation
 from jass.game.game_state import GameState
+from tensorflow import keras
 
 from label_play import LabelPlay
 
 
-def get_train_data(lines):
+def get_train_data(line):
     data_array = []
     played_card_array = []
-    for line in lines:
-        data = json.loads(line)
-        state = GameState.from_json(data.get('obs', {}))
-        played_card = data.get('action', {})
-        played_card_array.append(played_card)
-        label_play = LabelPlay.get_label_play(state, played_card)
-        data_array.append(label_play.to_dataframe())
-    return [data_array, np.array(played_card_array, dtype=np.float32)]
+
+    data = json.loads(line)
+    state = GameState.from_json(data.get('obs', {}))
+
+    played_card = data.get('action', {})
+    played_card_array.append(keras.utils.to_categorical(played_card, num_classes=36))
+
+    label_play = LabelPlay.get_label_play(state, played_card)
+    data_array.append(label_play.to_dataframe())
+
+    # Convert the list of DataFrames to a single DataFrame
+    data_df = pd.concat(data_array, ignore_index=True)
+
+    # Convert the DataFrame to a numpy array and reshape it to have a shape of (82,)
+    data_array = data_df.values.reshape((82,))
+
+    return data_array, played_card_array
